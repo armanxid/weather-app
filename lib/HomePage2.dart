21ebdf2a1d6350042f46c47db31cae04f4bf6401
armanxid <nurarmana@gmail.com>
2023-04-18 07:32:52 +0700

    add tabbar

--- lib/HomePage2.dart
+++ lib/HomePage2.dart
@@ -11,10 +11,11 @@ class HomePage2 extends StatefulWidget {
   _HomePage2State createState() => _HomePage2State();
 }
 
-class _HomePage2State extends State<HomePage2> {
+class _HomePage2State extends State<HomePage2> with TickerProviderStateMixin {
   DropdownItem? selectedDropdownItem;
   List<DropdownItem> dropdownItems = [];
   List? dataWeather;
+  late TabController _tabController;
   // List<Weather>? listWeather = [];
   // WeatherRepository weatherRepository = WeatherRepository();
 
@@ -27,6 +28,13 @@ class _HomePage2State extends State<HomePage2> {
     _fetchData();
     // _getDataWeather();
     super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   Future<void> getJsonData(BuildContext context, String? id) async {
@@ -64,151 +72,131 @@ class _HomePage2State extends State<HomePage2> {
     return MaterialApp(
       title: 'Weather App',
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: <Color>[
-                Color.fromARGB(255, 99, 64, 255),
-                Color.fromARGB(255, 143, 87, 255),
-                Color.fromARGB(255, 174, 134, 253),
-              ], // Gradient from https://learnui.design/tools/gradient-generator.html
-              tileMode: TileMode.mirror,
-            ),
-          ),
-          padding: EdgeInsets.all(16.0),
+        body: SingleChildScrollView(
           child: Container(
-            alignment: Alignment.center,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 100,
-                ),
-                Center(
-                  child: DropdownButton<DropdownItem>(
-                    icon: Icon(
-                      Icons.arrow_drop_down,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                    ),
-                    style: TextStyle(fontSize: 20),
-                    alignment: Alignment.center,
-                    hint: Text('Pilih Kabupaten'),
-                    dropdownColor: Color.fromARGB(255, 145, 235, 247),
-                    value: selectedDropdownItem,
-                    items: dropdownItems.map((item) {
-                      return DropdownMenuItem<DropdownItem>(
-                        value: item,
-                        child: Text(
-                          item.kota,
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                        ),
-                      );
-                    }).toList(),
-                    onChanged: (item) {
-                      setState(() {
-                        selectedDropdownItem = item;
-                        _getDataWeather();
-                      });
-                    },
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: <Color>[
+                  Color.fromARGB(255, 99, 64, 255),
+                  Color.fromARGB(255, 143, 87, 255),
+                  Color.fromARGB(255, 174, 134, 253),
+                ], // Gradient from https://learnui.design/tools/gradient-generator.html
+                tileMode: TileMode.mirror,
+              ),
+            ),
+            padding: EdgeInsets.all(16.0),
+            child: Container(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 100,
                   ),
-                ),
-                Center(
-                  child: selectedDropdownItem != null
-                      ? Text(
-                          '${selectedDropdownItem!.kecamatan}',
+                  Center(
+                    child: DropdownButton<DropdownItem>(
+                      icon: Icon(
+                        Icons.arrow_drop_down,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                      style: TextStyle(fontSize: 20),
+                      alignment: Alignment.center,
+                      hint: Text('Pilih Kabupaten'),
+                      dropdownColor: Color.fromARGB(255, 145, 235, 247),
+                      value: selectedDropdownItem,
+                      items: dropdownItems.map((item) {
+                        return DropdownMenuItem<DropdownItem>(
+                          value: item,
+                          child: Text(
+                            item.kota,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (item) {
+                        setState(() {
+                          selectedDropdownItem = item;
+                          _getDataWeather();
+                        });
+                      },
+                    ),
+                  ),
+                  Center(
+                    child: selectedDropdownItem != null
+                        ? Text(
+                            '${selectedDropdownItem!.kecamatan}',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 18),
+                          )
+                        : Text(''),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  dataWeather == null
+                      ? Text(" ")
+                      : Text(
+                          '${dataWeather?[1]['tempC']}\u2103',
                           style: TextStyle(
                               color: Colors.white,
                               fontWeight: FontWeight.w400,
-                              fontSize: 18),
-                        )
-                      : Text(''),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                dataWeather == null
-                    ? Text(" ")
-                    : Text(
-                        '${dataWeather?[2]['tempC']}\u2103',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 100),
-                      ),
-                SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: dataWeather == null
+                              fontSize: 100),
+                        ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: dataWeather == null
+                        ? Text('')
+                        : Text(dataWeather?[1]['jamCuaca'],
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                            )),
+                  ),
+                  dataWeather == null
                       ? Text('')
-                      : Text(dataWeather?[2]['jamCuaca'],
+                      : Text(dataWeather?[1]['cuaca'],
                           style: TextStyle(
                             color: Colors.white,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 18,
                           )),
-                ),
-                dataWeather == null
-                    ? Text('')
-                    : Text(dataWeather?[2]['cuaca'],
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w700,
-                          fontSize: 18,
-                        )),
-                Center(
-                  child: dataWeather == null
-                      ? Center(child: CircularProgressIndicator())
-                      : Image.asset(
-                          weatherIcon(dataWeather?[2]['cuaca']),
-                          width: 150,
-                          height: 150,
+                  Center(
+                    child: dataWeather == null
+                        ? Center(child: CircularProgressIndicator())
+                        : Image.asset(
+                            weatherIcon(dataWeather?[1]['cuaca']),
+                            width: 150,
+                            height: 150,
+                          ),
+                  ),
+                  DefaultTabController(
+                    length: 2,
+                    child: Column(
+                      children: [
+                        TabBar(
+                          tabs: <Widget>[
+                            Tab(
+                              text: 'Hari ini',
+                            ),
+                            Tab(
+                              text: 'Besok',
+                            ),
+                          ],
                         ),
-                  // child: RefreshIndicator(
-                  //   onRefresh: _fetchData,
-                  //   child: dataWeather == null
-                  //       ? Center(child: CircularProgressIndicator())
-                  //       : ListView.builder(
-                  //           itemCount:
-                  //               dataWeather == null ? 0 : dataWeather!.length,
-                  //           itemBuilder: (BuildContext context, int index) {
-                  //             return Container(
-                  //               padding: EdgeInsets.all(5.0),
-                  //               child: Column(
-                  //                 children: [
-                  //                   GestureDetector(
-                  //                     onTap: () {
-                  //                       print(dataWeather![index]["jamCuaca"]);
-                  //                     },
-                  //                     child: Padding(
-                  //                       padding: EdgeInsets.all(16.0),
-                  //                       child: Row(
-                  //                         children: [
-                  //                           SizedBox(width: 10),
-                  //                           Column(
-                  //                             children: [
-                  //                               Text(dataWeather![index]
-                  //                                       ["cuaca"] +
-                  //                                   " " +
-                  //                                   dataWeather![index]["tempC"]),
-                  //                               Text(dataWeather![index]["tempF"])
-                  //                             ],
-                  //                           )
-                  //                         ],
-                  //                       ),
-                  //                     ),
-                  //                   ),
-                  //                   Divider()
-                  //                 ],
-                  //               ),
-                  //             );
-                  //           }),
-                  // ),
-                ),
-              ],
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
